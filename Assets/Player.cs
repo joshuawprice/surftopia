ec594e549a1f38c1d0050017f020b59320a42da9
Joshua Price <git@jprice.uk>
2024-04-23 00:54:25 +0100

    First person mouse movement working

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,60 +1,90 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.Controls;
 
 public class Movement : MonoBehaviour
 {
-    MovementControls actions;
-    Vector3 moveInput;
+    PlayerControls controls;
     Rigidbody rb;
-    bool isJumping;
+
+    public float speed = 5.0f;
+    public float mouseSensitivity = 0.1f;
+
+    Vector2 moveInput;
+    Vector2 lookInput;
+    float xRotation = 0f;
+
+    bool isJumping = false;
 
     void Awake()
     {
-        actions = new MovementControls();
-
-        actions.gameplay.move.performed += OnMove;
-        actions.gameplay.move.canceled += OnMove;
-
-        actions.gameplay.jump.performed += OnJump;
-
+        controls = new PlayerControls();
         rb = GetComponent<Rigidbody>();
+
+        controls.gameplay.move.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
+        controls.gameplay.move.canceled += ctx => moveInput = Vector2.zero;
+        controls.gameplay.jump.performed += OnJump;
+
+        controls.gameplay.look.performed += ctx => lookInput = ctx.ReadValue<Vector2>();
+        controls.gameplay.look.canceled += ctx => lookInput = Vector2.zero;
     }
 
     // Start is called before the first frame update
-    void Start() { }
-    // Update is called once per frame
-    void Update() { }
-
-    void OnMove(InputAction.CallbackContext context)
+    void Start()
     {
-        if (context.phase == InputActionPhase.Performed)
-        {
-            Vector2 inputVector = context.ReadValue<Vector2>();
-            moveInput = new Vector3(inputVector.x, 0, inputVector.y);
-        }
-        else if (context.phase == InputActionPhase.Canceled)
-        {
-            moveInput = Vector3.zero;
-        }
+        // Lock the cursor to the center of the screen
+        Cursor.lockState = CursorLockMode.Locked;
+
+        // Hide the cursor from view
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = lookInput.x * mouseSensitivity;
+        float mouseY = lookInput.y * mouseSensitivity;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localEulerAngles = new Vector3(0f, transform.localEulerAngles.y + mouseX, 0f);
+        Camera.main.transform.localEulerAngles = new Vector3(xRotation, 0f, 0f);
     }
 
     void OnJump(InputAction.CallbackContext context)
     {
-        isJumping = true;
+        if (IsGrounded())
+        {
+            isJumping = true;
+        }
     }
 
     private void FixedUpdate()
     {
-        rb.MovePosition(rb.position + moveInput * 5.0f * Time.fixedDeltaTime);
+        Vector3 move = new Vector3(moveInput.x, 0, moveInput.y);
+        rb.MovePosition(rb.position + transform.TransformDirection(move) * speed * Time.fixedDeltaTime);
+
+        if (isJumping)
+        {
+            rb.AddForce(new Vector3(0, 5, 0), ForceMode.Impulse);
+            isJumping = false;
+        }
+    }
+
+    // Utility method to check if the player is on the ground
+    private bool IsGrounded()
+    {
+        // Checks if there's a collision on the player's feet
+        return Physics.Raycast(transform.position, Vector3.down, 0.6f);
     }
 
     void OnEnable()
     {
-        actions.gameplay.Enable();
+        controls.gameplay.Enable();
     }
+
     void OnDisable()
     {
-        actions.gameplay.Disable();
+        controls.gameplay.Disable();
     }
 }
